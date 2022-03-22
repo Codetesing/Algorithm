a7c226648febc6798624b0009c11efdcbd38f481
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-03-22 17:34:55 +0900

    BOJ_별 찍기 - 5
    https://www.acmicpc.net/problem/2442

---
+++ Implementation/EX-2442.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N; cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < N - i; j++)
+			printf(" ");
+		printf("*");
+		for (int j = 0; j < i - 1; j++)
+			printf("*");
+		for (int j = 0; j < i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
