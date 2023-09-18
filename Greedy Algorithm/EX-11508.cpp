9b25eadb2ac4cdfc7d962ce871839b2f64e3b586
Codetesting <37256601+Codetesing@users.noreply.github.com>
2023-09-18 13:37:33 +0900

    BOJ_2+1 세일
    https://www.acmicpc.net/problem/11508

---
+++ Greedy Algorithm/EX-11508.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N; cin >> N;
+    vector<int> v(N);
+    for (int i = 0; i < N; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    long long ans = 0;
+    for (int i = 0; i < N; i++) {
+        if (i % 3 != 2) {
+            ans += v[i];
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
