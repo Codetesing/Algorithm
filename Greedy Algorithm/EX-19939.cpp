4ce427c1d603623e05154812385247c8c36968dc
Codetesting <37256601+Codetesing@users.noreply.github.com>
2023-09-19 22:17:40 +0900

    BOJ_박 터뜨리기
    https://www.acmicpc.net/problem/19939

---
+++ Greedy Algorithm/EX-19939.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N, K; cin >> N >> K;
+
+    int ans = 0;
+    if (2 * N < K * (K + 1)) {
+        ans = -1;
+    }
+    else if ((2 * N - (K * (K + 1))) % K == 0) {
+        ans = K - 1;
+    }
+    else {
+        ans = K;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
