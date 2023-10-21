fd4b1bf312a22cfbc7d7e3ce91e46989281ed135
Codetesting <37256601+Codetesing@users.noreply.github.com>
2023-10-21 18:29:44 +0900

    BOJ_1998년생인 내가 태국에서는 2541년생?!
    https://www.acmicpc.net/problem/18108

---
+++ mathmatics/EX-18108.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int y; cin >> y;
+    cout << y - 543 << '\n';
+
+    return 0;
+}
